580a039b0d879b961d1fdedc7c26a8bdbaca9bd8
Yamuna Navada <yamunanavada@gmail.com>
2017-11-02 21:07:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   else
     string = "The line is currently: "
     array.each_with_index do |name, index|
-      string+= "#{index+1}: #{name}"
+      string+= "#{index+1}. #{name}"
       if index == array.length-1
         string+= "."
       else
