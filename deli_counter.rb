589ce5e4c3a818ac448ce01d75191fb57cb9e5dd
Yamuna Navada <yamunanavada@gmail.com>
2017-11-02 21:04:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently: "
+    array.each_with_index do |name, index|
+      string+= "#{index+1}: #{name}"
+      if index == array.length-1
+        string+= ". "
+      else
+        string+= ", "
+      end
+    end
+  end
+end
